825bc9c6e0950e7025fa5e5a09594bb3567bf087
saurookadook <maskiella@gmail.com>
2018-03-24 11:05:43 -0400

    Add sessions controller and user model

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'welcome#home'
+
+  devise_for :users
+
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
 end
