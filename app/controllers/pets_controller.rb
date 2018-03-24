24871c86d74e86ff74063801603bcd6c04dfebea
saurookadook <maskiella@gmail.com>
2018-03-24 17:51:19 -0400

    Fix seed data, stub out PetsController, migrate and seed database:

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,2 +1,26 @@
 class PetsController < ApplicationController
+  def index
+    @pets = Pet.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def adopt
+  end
+
+  def destroy
+  end
 end
