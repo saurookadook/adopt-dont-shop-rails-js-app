603964e2589d409b2f44105ca087bdd2b8a8ed29
saurookadook <maskiella@gmail.com>
2018-03-30 10:06:59 -0400

    Add note to self in pets/show view and generate necessary files for Employees

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'employees/index'
+
+  get 'employees/show'
+
+  get 'employees/new'
+
+  get 'employees/edit'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'welcome#home'
