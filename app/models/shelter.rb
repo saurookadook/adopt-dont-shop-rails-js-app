66fe9097a511f3ba897760b05a295f58d2c8bac7
saurookadook <maskiella@gmail.com>
2018-03-31 18:45:38 -0400

    Continue work on adding Address forms to views

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -3,6 +3,8 @@ class Shelter < ApplicationRecord
   has_many :pets
   has_one :address
 
+  accepts_nested_attributes_for :address
+
   validates :name, presence: true, uniqueness: true
   validates :address_id, presence: true
 
