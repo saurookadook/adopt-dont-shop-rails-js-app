1f293dbbed7402ab1b7c19b49242766db4e72031
saurookadook <maskiella@gmail.com>
2018-04-04 13:43:47 -0400

    Set up Pet associations, make basic layout for Employee show view

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,2 +1,9 @@
 class Pet < ApplicationRecord
+  has_many :caretakers
+  has_many :employees, through: :caretakers
+  belongs_to :user
+  belongs_to :shelter
+
+  validates :name, presence: true
+  validates :animal, presence: true
 end
