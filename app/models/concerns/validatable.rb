c54575a2d35a22d674b20bb277167c13d7a497dc
saurookadook <maskiella@gmail.com>
2018-04-05 17:07:07 -0400

    Clean up validatable module, fix password validation

--- app/models/concerns/validatable.rb
+++ app/models/concerns/validatable.rb
@@ -2,11 +2,9 @@ module Validatable
   extend ActiveSupport::Concern
 
   included do
-    validates :first_name, presence: true
-    validates :last_name, presence: true
-    validates :username, uniqueness: true, presence: true
-    validates :email, uniqueness: true, presence: true
-    validates :password, length: { in: 6..20 }
+    validates :first_name, :last_name, presence: true
+    validates :username, :email, uniqueness: true, presence: true
+    validates :password, length: { in: 6..20 }, on: :create
     has_secure_password
   end
 end
