21d9f22ad38fd54f71f96c5a7d3cf41c3a6ca60e
saurookadook <maskiella@gmail.com>
2018-05-22 12:26:38 -0400

    Work on necessary JS functions

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def home
+    @list_1_counter = 1
+    @list_2_counter = 1
     # set variables for displaying on page
     @shelter1 = Shelter.order(created_at: :desc)[0]
     @shelter2 = Shelter.order(created_at: :desc)[1]
