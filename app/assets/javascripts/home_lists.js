35fc2356713c53e1239962061ab05854387e006c
saurookadook <maskiella@gmail.com>
2018-05-22 19:44:59 -0400

    Sort out issues with displaying information correctly

--- app/assets/javascripts/home_lists.js
+++ app/assets/javascripts/home_lists.js
@@ -5,18 +5,18 @@ $(document).ready(function () {
 });
 
 function attachListeners () {
-  $('#shelter-list').on('click', () => displayShelters());
-  $('#recent-pets').on('click', () => displayRecentPets());
+  $('#shelter-list').on('click', () => loadShelters());
+  $('#recent-pets').on('click', () => loadRecentPets());
 }
 
 // perhaps loadShelterList would be better, and it would push shelter objects into shelterList variable?
 
 function displayShelters () {
-  let id = $(this).data("id");
-  $.get("/" + id + ".json", function (data) {
-    $(".shelterName").text()
-  })
+  $.post('/', data, function (shelters) {
+    let initialShelters = shelters.data.slice(0, 2);
+    $('.most-recent-shelters')
   // use to_json to load properties correctly
+  })
 }
 
 function displayRecentPets () {
