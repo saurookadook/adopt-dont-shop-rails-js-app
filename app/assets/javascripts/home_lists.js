e703374ff8d829e10c75e64bec571340aae6317b
saurookadook <maskiella@gmail.com>
2018-05-23 15:42:29 -0400

    Set up initial functions and prototype for shelters on index page

--- app/assets/javascripts/home_lists.js
+++ app/assets/javascripts/home_lists.js
@@ -1,6 +1,8 @@
-let shelterList = [];
+
+
 
 $(document).ready(function () {
+  initialShelters();
   attachListeners();
 });
 
@@ -11,9 +13,18 @@ function attachListeners () {
 
 function initialShelters () {
   $.getJSON(this.href).done(function(data) {
-    
-  })
-}
+    $('#most-recent-shelters').html('');
+    data.forEach(function(shelter) {
+      let counter = 0;
+      if (counter < 3) {
+        let newShelter = new Shelter(this.id, this.name, this.email, this.phoneNumber, this.address, this.employees, this.pets);
+        let formattedShelter = newShelter.formatShelterIndex();
+        $('#most-recent-shelters').append(formattedShelter);
+        counter++;
+      }
+    });
+  });
+};
 
 // Make prototypes
 function Shelter(id, name, email, phoneNumber, address, employees, pets) {
@@ -27,9 +38,12 @@ function Shelter(id, name, email, phoneNumber, address, employees, pets) {
 }
 
 // prototype to format shelter
-Feature.protoype.formatShelterIndex = function () {
+Shelter.prototype.formatShelterIndex = function () {
   let shelterHtml = '';
-  shelterHtml += `<h4 class="shelterName" data-id="${this.id}">${this.name}</h4>`
-  shelterHtml += `<p class="shelterAddress">${this.address.city}, ${this.address.state}</p>`
-  shelterHtml += `<a href="/shelters/${this.id}">Learn more</a>`
+  shelterHtml += '<div class="shelter-bubble bg-secondary">';
+  shelterHtml += `<h4 class="shelterName" data-id="${this.id}">${this.name}</h4>`;
+  shelterHtml += `<p class="shelterAddress">${this.address.city}, ${this.address.state}</p>`;
+  shelterHtml += `<a href="/shelters/${this.id}">Learn more</a>`;
+  shelterHtml += '</div>';
+  return shelterHtml;
 }
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,13 +1,9 @@
 class WelcomeController < ApplicationController
   def home
-    @list_1_counter = 1
-    @list_2_counter = 1
-    # set variables for displaying on page
-    @shelter1 = Shelter.order(created_at: :desc)[0]
-    @shelter2 = Shelter.order(created_at: :desc)[1]
-    @shelter3 = Shelter.order(created_at: :desc)[2]
-
-    # @shelters = Shelter.order(created_at: :desc)
-    # render json: @shelters
+    @shelters = Shelter.order(created_at: :desc)
+    respond_to do |format|
+      format.html { render :home }
+      format.html { render json: @shelters }
+    end
   end
 end
