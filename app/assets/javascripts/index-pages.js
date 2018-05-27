8cb3bf54cb62e180cec99ad4b573e02faaa0b713
saurookadook <maskiella@gmail.com>
2018-05-27 15:48:53 -0400

    Fleshed out functional (hopefully) formatShelterIndex prototype

--- app/assets/javascripts/index-pages.js
+++ app/assets/javascripts/index-pages.js
@@ -24,5 +24,21 @@ function Shelter(id, name, email, phoneNumber, address) {
 
 Shelter.prototype.formatShelterIndex = function () {
   let shelterIndexHtml = '';
+  // find out way to use `display_address` helper
+  if (this.street2 != "") {
+    shelterIndexHtml += `${this.street1}<br>${this.street2}<br>${this.city}, ${this.state} ${this.zip}`
+  } else if (this.street1 != "" && this.zip != "") {
+    shelterIndexHtml += `${this.street1}<br>${this.city}, ${this.state} ${this.zip}`
+  } else {
+    shelterIndexHtml += `${this.city}, ${this.state}`
+  }
+  shelterIndexHtml += "<br>"
+  if (this.phoneNumber != null && this.email != null) {
+    shelterIndexHtml += `Phone Number: ${this.phoneNumber}<br>Email: ${this.email}`
+  } else if (this.email != null) {
+    shelterIndexHtml += `Email: ${this.email}`
+  } else if (this.phoneNumber != null) {
+    shelterIndexHtml += `Phone Number: ${this.phoneNumber}`
+  }
   return shelterIndexHtml;
 }
