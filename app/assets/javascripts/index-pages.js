4b48637a9b89e5f87ff1f2590cd31b47fa897c77
saurookadook <maskiella@gmail.com>
2018-05-27 01:21:38 -0400

    Additional styling to home page and shelters index page

--- app/assets/javascripts/index-pages.js
+++ app/assets/javascripts/index-pages.js
@@ -1,23 +1,23 @@
-$(document).ready(function () {
-  attachIndexListeners();
-})
-
-function attachIndexListeners () {
-  $('.js-next').on('click', loadNextShelters)
-}
-
-function loadNextShelters (e) {
-  e.preventDefault();
-}
-
-function Shelter(id, name, email, phoneNumber, address) {
-  this.id = id;
-  this.name = name;
-  this.email = email;
-  this.phoneNumber = phoneNumber;
-  this.address = address;
-}
-
-Shelter.prototype.formatShelterIndex = function () {
-  let shelterIndexHtml = '';
-}
+// $(document).ready(function () {
+//   attachIndexListeners();
+// })
+//
+// function attachIndexListeners () {
+//   $('.js-next').on('click', loadNextShelters)
+// }
+//
+// function loadNextShelters (e) {
+//   e.preventDefault();
+// }
+//
+// function Shelter(id, name, email, phoneNumber, address) {
+//   this.id = id;
+//   this.name = name;
+//   this.email = email;
+//   this.phoneNumber = phoneNumber;
+//   this.address = address;
+// }
+//
+// Shelter.prototype.formatShelterIndex = function () {
+//   let shelterIndexHtml = '';
+// }
